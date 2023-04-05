96b4ce8a274b9a75938b19c798176899b5fc3b23
7K <kmanfinkel17@gmail.com>
2023-04-05 15:21:37 -0500

    "added character variables"

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -14,17 +14,21 @@ function generatePassword() {
  var passwordLength = prompt("How many characters would you like your password to be? (between 8-128)");
  if (passwordLength || passwordLength < 8 || passwordLength > 128) {
   alert("Password length must be a number between 8 and 128");
-  return generatePassword();
+  return ;
  }
 
  var addLowercase = confirm("Would you like to add lowercase characters?");
  var addUppercase = confirm("Would you like to add UPPERCASE characters?");
  var addNumbers = confirm("W0u1d y0u 1ik3 t0 add numb3r character5?");
  var addSpecial = confirm("Would you like to add special (/?!@#) characters?");
+ var lowercaseCharset = "abcdefghijklmnopqrstuvwxyz";
+ var uppercaseCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ var numberCharset = "0123456789";
+ var specialCharset = "!@#$%^&*()[]\{}|;:',.<></>`~=+-";
 
  if (!addLowercase && !addUppercase && !addNumbers && !addSpecial) {
   alert(" You must choose at least one character type to continue");
-  return "";
+  return ;
  }
 
 };
