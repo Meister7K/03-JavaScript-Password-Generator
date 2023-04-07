c42edadcac9605d49e3f970a4c67fc78e8bc1bb0
7K <kmanfinkel17@gmail.com>
2023-04-07 16:57:38 -0500

    "formatted JS file and added readme info"

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
 # 03-JavaScript-Password-Generator
 
 ## Description
-- This project was built to create an app that could create a randomly generated password. the page uses promps and confirms to select length of password and different characters included in the generated password. I learned about creating variables and pulling data from them along with the use of functions and for loops to make recursive processes automated. 
 
+- This project was built to create an app that could create a randomly generated password. the page uses promps and confirms to select length of password and different characters included in the generated password. I learned about creating variables and pulling data from them along with the use of functions and for loops to make recursive processes automated.
 
 ## Link
+
 - [link](https://meister7k.github.io/03-JavaScript-Password-Generator/)
+
 ## Screenshot
+
 ![Photo](./assets/images/passwordGenerator-03.png)
 
-
-
-
-
 ## Cited
+
 - youtube/ChatGPT
-    - [Link](https://www.youtube.com/shorts/sWksH_V0SAQ)
-    - John Komarnicki - [link](https://www.youtube.com/channel/UCr0D7PVNOHqB3Td7lVl_LKw/join)
 
-- JavaScript "the comprehensive guide" by Phillip Ackermann, 
-2022 Rheinwerk_Publishing.inc Boston, MA pg.115-127, 313, 
+  - [Link](https://www.youtube.com/shorts/sWksH_V0SAQ)
+  - John Komarnicki - [link](https://www.youtube.com/channel/UCr0D7PVNOHqB3Td7lVl_LKw/join)
 
-- htmlcheatsheet.com/js https://htmlcheatsheet.com/js/  
+- JavaScript "the comprehensive guide" by Phillip Ackermann,
+  2022 Rheinwerk_Publishing.inc Boston, MA pg.115-127, 313,
+
+- htmlcheatsheet.com/js https://htmlcheatsheet.com/js/
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -8,61 +8,70 @@ function writePassword() {
 
   passwordText.value = password;
   // return document.getElementById(password).append.innerHTML.password
-};
+}
 
 function generatePassword() {
- var passwordLength = prompt("How many characters would you like your password to be? (between 8-128)");
- if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-  alert("Password length must be a NUMBER between 8 and 128");
-  console.log(passwordLength);
-  return "";
- }
+  var passwordLength = prompt(
+    "How many characters would you like your password to be? (between 8-128)"
+  );
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Password length must be a NUMBER between 8 and 128");
+    console.log(passwordLength);
+    return "";
+  }
 
-//  
- var addLowercase = confirm("Would you like to add lowercase characters?");
- var addUppercase = confirm("Would you like to add UPPERCASE characters?");
- var addNumbers = confirm("W0u1d y0u 1ik3 t0 add numb3r character5?");
- var addSpecial = confirm("Would you like to add special (/?!@#) characters?");
- var lowercaseCharset = "abcdefghijklmnopqrstuvwxyz";
- var uppercaseCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
- var numberCharset = "0123456789";
- var specialCharset = "!@#$%^&*()[]{}|;:',.<></>`~=+-";
- var passwordText = "";
- var charset = "";
+  //
+  var addLowercase = confirm("Would you like to add lowercase characters?");
+  var addUppercase = confirm("Would you like to add UPPERCASE characters?");
+  var addNumbers = confirm("W0u1d y0u 1ik3 t0 add numb3r character5?");
+  var addSpecial = confirm("Would you like to add special (/?!@#) characters?");
+  var lowercaseCharset = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numberCharset = "0123456789";
+  var specialCharset = "!@#$%^&*()[]{}|;:',.<></>`~=+-";
+  var passwordText = "";
+  var charset = "";
 
+  //  alert of failed char selection
+  if (!addLowercase && !addUppercase && !addNumbers && !addSpecial) {
+    alert(" You must choose at least one character type to continue");
+    return "";
+  }
 
-//  alert of failed char selection
- if (!addLowercase && !addUppercase && !addNumbers && !addSpecial) {
-  alert(" You must choose at least one character type to continue");
-  return "";
- }
+  //  code for choosing random characters from charset based on input
+  if (addLowercase) {
+    charset += lowercaseCharset;
+    passwordText += lowercaseCharset.charAt(
+      Math.floor(Math.random() * lowercaseCharset.length)
+    );
+  }
+  if (addUppercase) {
+    charset += uppercaseCharset;
+    passwordText += uppercaseCharset.charAt(
+      Math.floor(Math.random() * uppercaseCharset.length)
+    );
+  }
+  if (addNumbers) {
+    charset += numberCharset;
+    passwordText += numberCharset.charAt(
+      Math.floor(Math.random() * numberCharset.length)
+    );
+  }
+  if (addSpecial) {
+    charset += specialCharset;
+    passwordText += specialCharset.charAt(
+      Math.floor(Math.random() * specialCharset.length)
+    );
+  }
 
-//  code for choosing random characters from charset based on input
- if(addLowercase) {
-  charset += lowercaseCharset;
-  passwordText += lowercaseCharset.charAt(Math.floor(Math.random() * lowercaseCharset.length));
- }
- if(addUppercase){
-  charset += uppercaseCharset;
-  passwordText += uppercaseCharset.charAt(Math.floor(Math.random() * uppercaseCharset.length));
- }
- if(addNumbers){
-  charset += numberCharset;
-  passwordText += numberCharset.charAt(Math.floor(Math.random() * numberCharset.length));
- }
- if(addSpecial){
-  charset += specialCharset;
-  passwordText += specialCharset.charAt(Math.floor(Math.random() * specialCharset.length));
- }
+  //  for loop to randomize each character/charset in character length
+  for (var i = passwordText.length; i < passwordLength; i++) {
+    passwordText += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
 
-//  for loop to randomize each character/charset in character length 
- for (var i = passwordText.length; i < passwordLength; i++ ){
-  passwordText += charset.charAt(Math.floor(Math.random() * charset.length));
- }
-
- console.log(passwordText);
- console.log(charset)
- return passwordText;
-};
+  console.log(passwordText);
+  console.log(charset);
+  return passwordText;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
